f69c994d2a93a81cc3be111e8b08f05388367d71
r4lfl4b <dev@rgalan.es>
2019-11-10 17:52:43 +0000

    feat: add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.scss";
 import TodoForm from "./components/todoForm/TodoForm";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="todo-container">
-      <TodoForm />
+      <Switch>
+        <Route path="/" exact>
+          <h1>HOME</h1>
+        </Route>
+        <Route path="/add">
+          <TodoForm />
+        </Route>
+      </Switch>
     </div>
   );
 }
