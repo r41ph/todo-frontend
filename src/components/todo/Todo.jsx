6bc561ff24db74afc5013e212fed008cae80d4f6
r4lfl4b <dev@rgalan.es>
2019-11-11 11:50:34 +0000

    feat: add edit button

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "./Todo.scss";
+import Button from "../button/Button";
 
 const propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string,
   duedate: PropTypes.string
 };
 
-const Todo = ({ title, description, duedate }) => {
+const Todo = ({ id, title, description, duedate }) => {
   return (
     <div className="todo-container">
-      <div className="todo-title">{title}</div>
-      <div className="todo-duedate">
-        {" "}
-        {duedate ? `Due Date: ${duedate}` : ""}
+      <div className="todo-toggle">
+        <div className="todo-title">{title}</div>
+        <div className="todo-options">
+          <Button>
+            <Link to={`/update/${id}`}>Edit</Link>
+          </Button>
+        </div>
+        <div className="todo-duedate">
+          {duedate ? `Due Date: ${duedate}` : ""}
+        </div>
       </div>
       <div className="todo-description">Description: {description}</div>
     </div>
