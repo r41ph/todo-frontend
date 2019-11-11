28dc85550d55b3d7d6d7440e106ecfb0d6f034c8
r4lfl4b <dev@rgalan.es>
2019-11-11 09:16:45 +0000

    refact: move Todos fetch to parent component (App)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from "react";
+import axios from "axios";
 import "./App.scss";
-import TodoForm from "./components/todoForm/TodoForm";
 import { Switch, Route } from "react-router-dom";
 import TodoList from "./components/todoList/TodoList";
+import AddTodo from "./components/addTodo/AddTodo";
+import UpdateTodo from "./components/updateTodo/UpdateTodo";
 
 function App() {
+  const [todos, setTodos] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/todos")
+      .then(res => setTodos(res.data))
+      .catch(err => console.log("err", err));
+  }, []);
+
   return (
     <div className="todos-container">
       <Switch>
         <Route path="/" exact>
-          <TodoList />
+          <TodoList todos={todos} />
         </Route>
         <Route path="/add">
-          <TodoForm />
+          <AddTodo />
         </Route>
+        <Route
+          path="/update/:id"
+          render={props => <UpdateTodo {...props} todos={todos} />}
+        ></Route>
       </Switch>
     </div>
   );
--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,38 +1,26 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 import "./TodoList.scss";
 import moment from "moment";
 import Todo from "../todo/Todo";
 
-const TodoList = () => {
-  const [todos, setTodos] = React.useState([]);
-  React.useEffect(() => {
-    axios
-      .get("http://localhost:3001/api/todos")
-      .then(res => setTodos(res.data))
-      .catch(err => console.log("err", err));
-  }, []);
-
-  return (
-    <div className="todo-container">
-      {todos.length > 0
-        ? todos.map(todo => (
-            <Todo
-              key={todo._id}
-              title={todo.todo_title}
-              description={todo.todo_description}
-              duedate={
-                todo.todo_duedate !== ""
-                  ? moment(todo.todo_duedate).format(
-                      "dddd, Do MMMM YYYY, h:mm:ss a"
-                    )
-                  : ""
-              }
-            />
-          ))
-        : "Loading"}
-    </div>
-  );
-};
+const TodoList = ({ todos = [] }) => (
+  <div className="todo-container">
+    {todos.length > 0
+      ? todos.map(todo => (
+          <Todo
+            key={todo._id}
+            title={todo.todo_title}
+            description={todo.todo_description}
+            duedate={
+              todo.todo_duedate !== ""
+                ? moment(todo.todo_duedate).format("dddd, Do MMMM YYYY, h:mm a")
+                : ""
+            }
+          />
+        ))
+      : "Loading"}
+  </div>
+);
 
 export default TodoList;
