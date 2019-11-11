794039bcf647cfd393b91acec7808fed76cd7fa4
r4lfl4b <dev@rgalan.es>
2019-11-11 17:33:48 +0000

    fix: show message when no todos to display

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -15,30 +15,35 @@ const TodoList = ({ todos = [], updateTodos }) => {
     updateTodos(updatedTodos);
   };
 
+  const renderTodos = () => {
+    const todosList = todos.reduce((acc, todo) => {
+      if (todo.todo_completed === false) {
+        acc.push(
+          <Todo
+            key={todo._id}
+            id={todo._id}
+            title={todo.todo_title}
+            description={todo.todo_description}
+            duedate={
+              todo.todo_duedate !== ""
+                ? moment(todo.todo_duedate).format("M/D/Y h:mm A")
+                : ""
+            }
+            completed={todo.todo_completed}
+            deleteTodo={onDeleteTodo}
+            updateStatus={onUpdateTodoStatus}
+          />
+        );
+      }
+      return acc;
+    }, []);
+
+    return todosList.length > 0 ? todosList : "There are no TODOs!";
+  };
+
   return (
     <div className="todos-container">
-      {todos.length > 0
-        ? todos.map(todo =>
-            todo.todo_completed !== true ? (
-              <Todo
-                key={todo._id}
-                id={todo._id}
-                title={todo.todo_title}
-                description={todo.todo_description}
-                duedate={
-                  todo.todo_duedate !== ""
-                    ? moment(todo.todo_duedate).format("M/D/Y h:mm A")
-                    : ""
-                }
-                completed={todo.todo_completed}
-                deleteTodo={onDeleteTodo}
-                updateStatus={onUpdateTodoStatus}
-              />
-            ) : (
-              ""
-            )
-          )
-        : "Loading"}
+      {todos.length > 0 ? renderTodos() : "Loading"}
     </div>
   );
 };
