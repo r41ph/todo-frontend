3c56d790a830e93a6678ae2290f156fbb14bcc7a
r4lfl4b <dev@rgalan.es>
2019-11-11 11:49:19 +0000

    feat: update local state on update Todo

--- src/components/updateTodo/UpdateTodo.jsx
+++ src/components/updateTodo/UpdateTodo.jsx
@@ -4,6 +4,7 @@ import "./UpdateTodo.scss";
 import TodoForm from "../todoForm/TodoForm";
 
 const UpdateTodo = props => {
+  const { updateTodos, todos } = props;
   const handleSubmit = (title, description, datetime, id) => {
     if (title === "") {
       return;
@@ -16,11 +17,14 @@ const UpdateTodo = props => {
       todo_completed: false
     };
 
-    console.log("todoDetails", todoDetails);
-
     axios
       .post(`http://localhost:3001/api/todos/update/${id}`, todoDetails)
-      .then(res => console.log(res.data));
+      .then(res => {
+        const updatedTodos = todos.map(todo => {
+          return todo._id === id ? res.data : todo;
+        });
+        updateTodos(updatedTodos);
+      });
   };
 
   return <TodoForm onHandleSubmit={handleSubmit} {...props} />;
