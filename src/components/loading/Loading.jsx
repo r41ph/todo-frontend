b744e638a4a4add4e9ba71cbb962ce81ac927328
r4lfl4b <dev@rgalan.es>
2019-11-13 16:14:07 +0000

    feat: add loading component

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="todo-loading">
+      <span className="todo-loading_circle"></span>
+    </div>
+  );
+};
+
+export default Loading;
